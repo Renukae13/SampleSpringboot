6a1a6e7a78ff9799393ea8a1fe0f8320949ef89f
kavya <reddymadhu12@gmail.com>
2022-06-29 23:18:54 +0530

    test

--- src/main/java/com/javabycode/springboot/HelloWorldController.java
+++ src/main/java/com/javabycode/springboot/HelloWorldController.java
@@ -1,18 +1,16 @@
 package com.javabycode.springboot;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public String hello() {
         
-        String message="You just create Spring Boot Example successfully";
-        model.addAttribute("name", name);
-        model.addAttribute("message", message);
         
         return "hello";
     }
